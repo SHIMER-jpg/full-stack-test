29ae0fe47e57251475848adb40ab8f0614890790
Santiago DIaz <73915679+SHIMER-jpg@users.noreply.github.com>
2022-12-26 21:55:06 -0300

    removed unnecesary files

--- cli/webpack.config.js
+++ cli/webpack.config.js
@@ -8,24 +8,7 @@ module.exports = {
     filename: "main.js",
   },
   target: "web",
-  // devServer: {
-  //   port: "3000",
-  //   /** "static"
-  //    * This property tells Webpack what static file it should serve
-  //    */
-  //   static: ["./public"],
-  //   open: true,
-  //   hot: true,
-  //   liveReload: true,
-  // },
   module: {
-    /** "rules"
-     * This says - "Hey webpack compiler, when you come across a path that resolves to a '.js or .jsx'
-     * file inside of a require()/import statement, use the babel-loader to transform it before you
-     * add it to the bundle. And in this process, kindly make sure to exclude node_modules folder from
-     * being searched"
-     */
-
     rules: [
       {
         test: /\.(js|jsx)$/, // kind of file extension this rule should look for and apply in test
